2be368d09bac7dcc37ea24fa4aa689f019ac5b6b
dondakeshimo <went.went.takkun135@gmail.com>
2021-04-29 21:23:44 +0900

    feat: add domain/feature interface

---
+++ src/domain/feature/feature_interface.py
@@ -0,0 +1,28 @@
+"""featureモジュールのクラスのインタフェースを規定する
+
+    featureモジュール内のクラスはこのモジュールのクラスを継承している
+"""
+
+from abc import ABC, abstractmethod
+from typing import List
+import pandas as pd
+
+
+class FeatureInterface(ABC):
+    _df: pd.DataFrame
+
+    @abstractmethod
+    def load_data(self, path: List[str]) -> None:
+        pass
+
+    @abstractmethod
+    def preprocess(self) -> None:
+        pass
+
+    @abstractmethod
+    def extract_feature(self) -> None:
+        pass
+
+    @property
+    def df(self) -> pd.DataFrame:
+        return self._df
