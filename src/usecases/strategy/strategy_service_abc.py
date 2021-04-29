e835871a6a393307973d189b56cea493da0ac713
dondakeshimo <went.went.takkun135@gmail.com>
2021-04-29 21:23:23 +0900

    feat: add usecase inteface

---
+++ src/usecases/strategy/strategy_service_abc.py
@@ -0,0 +1,21 @@
+"""StrategyServiceの抽象クラスを定義するモジュール
+
+    - StrategyServiceは投資戦略における手順を記載する
+    - 具体的なデータの操作や生成についてはdomain層で行う
+    - ハイパーパラメタなどの設定についてもdomain層で解釈する
+"""
+
+from abc import ABC, abstractmethod
+
+
+class StrategyService(ABC):
+    """投資戦略の手順を記載するクラスの親クラス
+    """
+
+    @abstractmethod
+    def select_code(self) -> None:
+        pass
+
+    @abstractmethod
+    def adjust_ratio(self) -> None:
+        pass
