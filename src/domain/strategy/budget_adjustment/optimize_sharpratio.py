07072594cf91e29208684819f1b68b25208c0469
kuchii <ohmyroimax@gmail.com>
2021-05-10 00:26:00 +0900

    refactor

--- src/domain/strategy/budget_adjustment/optimize_sharpratio.py
+++ src/domain/strategy/budget_adjustment/optimize_sharpratio.py
@@ -14,14 +14,16 @@ class BudgetAdjustor(BudgetAdjustorABC):
     def adjust(self, request: BudgetAdjustorRequest) -> None:
         """stock_dfにbudgetカラムを追加して比率を最適化する。
         """
+        digit = 1000000
+        MIN_BET = 10000
         labels_df = request.labels_df.copy()
-        purchase_df = request.stock_df.copy()
+        stock_df = request.stock_df.copy()
+
         # 5銘柄以上購入する必要があるので、30銘柄1万円は必ず購入する。
-        cash = 10000
+        stock_df.loc[:, "budget"] = MIN_BET
         # ポートフォリオ最適化で配分する総予算
-        TOTAL_BUDGET = 1000000 - cash * request.code_num
-        # 最適化できない場合もあるので、初期値として、均等に予算を割り当てる
-        purchase_code_list = purchase_df.loc[:, "code"].unique()
+        total_budget = 1000000 - MIN_BET * request.code_num
+        purchase_code_list = stock_df.loc[:, "code"].unique()
 
         # Pyportfolioの入力形式にdfを変更する。
         # indexにcode追加してmulti indexにする
@@ -29,33 +31,31 @@ class BudgetAdjustor(BudgetAdjustorABC):
                                         append=True).sort_index()
         # 対象の変化率だけ抜き取り、銘柄コードを列名に。
         labels_df = labels_df.loc[:, "label_high_5"].unstack("Local Code")
+
         #　期間指定
-        end_date = purchase_df.index.unique()[0]
+        end_date = stock_df.index.unique()[0]
         start_date = end_date - pd.offsets.BDay(90)
         labels_df = labels_df[start_date:end_date]
         # 銘柄指定
         labels_df = labels_df.loc[:, purchase_code_list]
+
         # 期待リターン
         mu = labels_df.mean()
         # 標本分散共分散行列
-        # std = risk_models.sample_cov(df)
         std = labels_df.dropna(how='all').cov()
-        # 下方半分散
-        # std = risk_models.semicovariance(df, benchmark=0)
-        # ef = EfficientFrontier(mu, std)
         cla = CLA(mu, std)
+
         # 最適化
         try:
             portfolio = cla.max_sharpe()
         except Exception as e:
-            return purchase_df
+            return stock_df
+
         # budgetを最適値で更新
-        digit = 1000000
         for code in purchase_code_list:
-            # 最適化計算時の数値誤差で1を超える場合があるので、小数点の切り上げ・切り捨てする。
+            # 最適化計算時の数値誤差で1を超える場合があるので、四捨五入する。
             ceiled_ratio = np.round(portfolio[code] * digit) / digit
-            purchase_df.loc[purchase_df["code"] == code,
-                            "budget"] = int(TOTAL_BUDGET * ceiled_ratio)
-        purchase_df.loc[:, "budget"] += cash
-        purchase_df.loc[:, "budget"] = purchase_df["budget"].astype(int)
-        return purchase_df
+            stock_df.loc[stock_df["code"] == code,
+                            "budget"] += int(total_budget * ceiled_ratio)
+        stock_df.loc[:, "budget"] = stock_df["budget"].astype(int)
+        return stock_df
